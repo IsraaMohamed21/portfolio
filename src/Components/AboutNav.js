c056ffd2f7caf789135d5f5df8143754fa6145c4
IsraaMohamed21 <168222737+IsraaMohamed21@users.noreply.github.com>
2025-08-07 07:48:50 +0300

    updated portfolio

---
+++ src/Components/AboutNav.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Education from "./Education";
+import Skills from "./Skills";
+
+export default function AboutNav() {
+  const [activeSection, setActiveSection] = useState("education"); // default section
+
+  return (
+    <div className="about-nav">
+      <ul className="about-nav-links">
+        <li>
+          <button onClick={() => setActiveSection("skills")}>Skills</button>
+        </li>
+        <li>
+          <button onClick={() => setActiveSection("education")}>Education</button>
+        </li>
+      </ul>
+
+      <div className="about-content">
+        {activeSection === "skills" && <Skills />}
+        {activeSection === "education" && <Education />}
+      </div>
+    </div>
+  );
+}
